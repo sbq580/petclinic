1a8d52ee1c76f38e9b242122b2a1a803df80efaa
Shabiq Hazarika <hazarika.shabiq@gmail.com>
2019-04-04 14:38:48 +0530

    Vet list page and controller added. Closes #7

---
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/VetController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author HazariSH
+ *
+ */
+@Controller
+public class VetController {
+	
+	@RequestMapping({"/vets",  "/vets/index", "/vets/index.html"})
+	public String listVets() {
+		
+		return "vets/index";
+	}
+}
